0b9392ec967b8ee1a64d4bbfdf6530b289adce1b
mohamed awad <mohamed.awad.elgammal@gmail.com>
2024-03-05 22:47:10 +0200

    add database and class DB

---
+++ db.php
@@ -0,0 +1,45 @@
+<?php
+class DB {
+    private $host = "localhost";
+    private $user = "root";
+    private $dbname = "cafeteria_DB";
+    private $password = "";
+    private $connection;
+
+    function __construct() {
+        $this->connection = new mysqli($this->host, $this->user, $this->password, $this->dbname);
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
+    }
+
+    function getData($tableName, $condition = "1") {
+        $result = $this->connection->query("SELECT * FROM $tableName WHERE $condition");
+        if (!$result) {
+            die("Error fetching data: " . $this->connection->error);
+        }
+        return $result;
+    }
+
+    function updateData($tableName, $setValues, $condition = "1") {
+        $sql = "UPDATE $tableName SET $setValues WHERE $condition";
+        if ($this->connection->query($sql) === FALSE) {
+            die("Error updating data: " . $this->connection->error);
+        }
+    }
+
+    function insertInto($tableName, $columnNames, $values) {
+        $sql = "INSERT INTO $tableName $columnNames VALUES $values";
+        if ($this->connection->query($sql) === FALSE) {
+            die("Error inserting data: " . $this->connection->error);
+        }
+    }
+
+    function delete($tableName, $condition = "1") {
+        $sql = "DELETE FROM $tableName WHERE $condition";
+        if ($this->connection->query($sql) === FALSE) {
+            die("Error deleting data: " . $this->connection->error);
+        }
+    }
+}
+?>
