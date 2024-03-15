73fa888f3de2ed9c14a4218552b50643ff28beda
Ameer-Elattar <amer.alattar.ae@gmail.com>
2024-03-15 20:20:56 +0200

    checks styling

--- checks/checks.php
+++ checks/checks.php
@@ -9,19 +9,42 @@ session_start();
         border-radius: 50%;
         height: 50px;
     }
-
+    body{
+    background-color: #F4EAE0 !important;
+    }
     .allproduct img {
         cursor: pointer;
         margin: auto;
         display: inline-block;
     }
+    .order-details > table > tbody > tr:hover{
+        background-color: #F4EAE0 !important;
+    }
+    .order-full-details > div{
+        text-align: center;
+    }
+    .order-full-details > div:hover{
+    background-color: #FAF6F0 !important;
+    transform: scale(1.2);
+    transition: transform 1.5s;
+    z-index: 1;
+    border:2px solid #000000;
+    border-radius: 10px;
+    }
+    .order-full-details{
+        margin-top: 5px;
+        /* border: 1px solid black; */
+        justify-content: space-around;
+        align-items: center;
+        display: none;
+    }
 </style>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <?PHP include "../components/nav.php" ?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <h2>Checks</h2>
+<div class="container row justify-content-between mx-auto">
+    <div class=" card h-50 shadow p-4 col-4">
+        <!-- <div class="col-md-6 offset-md-3"> -->
+            <h2 class="mx-auto">Checks</h2>
             <form>
                 <div class="form-row">
                     <div class="form-group col">
@@ -47,39 +70,30 @@ session_start();
                         style="height: fit-content;">Filter</button></div>
 
             </form>
-        </div>
+        <!-- </div> -->
     </div>
-    <div class="row mt-2">
-        <div class="col-md-6 offset-md-3">
-            <h2>User Orders</h2>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Total Price</th>
-                    </tr>
-                </thead>
-                <tbody id="userOrdersTableBody">
-                </tbody>
-            </table>
-            <div class="order-details" style="display:none;"></div>
-            <div class="order-full-details"></div>
-        </div>
+    <div class=" bg-light shadow-lg col-7">
+        
+            <div class="text-center">
+                <h2 >User Orders</h2>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Total Price</th>
+                        </tr>
+                    </thead>
+                    <tbody id="userOrdersTableBody">
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <div class="order-details" style="display:none;"></div>
+                <div class="order-full-details"></div>
+            </div>
     </div>
 </div>
 
-<style>
-    .order-details{
-        border: 1px solid black;
-    }
-    .order-full-details{
-        margin-top: 5px;
-        border: 1px solid black;
-        justify-content: space-around;
-        align-items: center;
-        display: none;
-    }
-</style>
 
 <script>
 // Retrieve user orders from PHP variable
