b2e8473db3a1dc0097f5fd303e17d47d2d055c31
ahmeddmahmoud <ahmed.mahmoudd.elshahat@gmail.com>
2024-03-16 00:29:10 +0200

    Updating Categories Fixing some bugs

--- categories/addCategory.php
+++ categories/addCategory.php
@@ -9,24 +9,27 @@ if ($_SERVER["REQUEST_METHOD"]=="POST"){
     if(isset($_POST['category'])){
         $category=trim($_POST['category']);
         if(preg_match("/[^a-zA-Z0-9_\s-]/",$category)){
-            setcookie("errMsg","Category cannot contain special characters!");
+            setcookie("errMsg","Category cannot contain special characters!", time() + 1, "/");
             header('Location: categoryForm.php');
             exit();
         }
         if(strlen($category)<3){
-            setcookie("errMsg","Category should be at least 3 characters!");
+            setcookie("errMsg","Category should be at least 3 characters!", time() + 1, "/");
             header('Location: categoryForm.php');
             exit();
         }
         $category=strtolower($category);
         $result = $db->getData("category", "name = '$category'");
         if ($result->num_rows > 0) {
-            setcookie("errMsg","Category already exists!");
+            setcookie("errMsg","Category already exists!", time() + 1, "/");
             header('Location: categoryForm.php');
             exit();
         }
         $db->insert_data("category","name", "'$category'");
-        header('Location: categoryForm.php');
+        //setcookie("successMsg","The Category Has been added successfully!");
+        setcookie("successMsg", "The Category Has been added successfully!", time() + 1, "/");
+
+        header('Location: ../products/productForm.php');
     }
 }
 ?>
