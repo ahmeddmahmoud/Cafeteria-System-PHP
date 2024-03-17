cb284af793f37eb95dc2b8f8a581084a25efb555
TalaatEysa <talaateysa@outlook.com>
2024-03-17 10:20:21 +0200

    modify line 5 in delete

--- products/deleteProduct.php
+++ products/deleteProduct.php
@@ -2,7 +2,7 @@
 include_once '../db.php'; // Include the DB class file
 $db=new DB();
 require_once '../functions/validateSourcePage.php';
-if (!isset($_POST['id'])) {
+if (!isset($_GET['id'])) {
     validateSourcePage('productForm.php', '../errors/err.php',403);
 }
 //query
